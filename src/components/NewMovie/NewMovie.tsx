4c037591b12826683e88344db0d738d778505fd5
Mark Chepizhny <markchep0411@gmail.com>
2026-01-16 20:52:55 +0200

    Solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,91 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { MovieField } from '../../constants/MovieField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAddMovie: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAddMovie = () => {} }) => {
+  const [movieData, setMovieData] = useState<Movie>({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const [formKey, setFormKey] = useState(0);
+
+  const handleChange = (field: keyof Movie) => (value: string) => {
+    setMovieData(prev => ({ ...prev, [field]: value }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    onAddMovie({ ...movieData });
+
+    setMovieData({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+
+    setFormKey(prev => prev + 1);
+  };
+
+  const isFormValid =
+    movieData[MovieField.IMDB_ID].trim() &&
+    movieData[MovieField.IMDB_URL].trim() &&
+    movieData[MovieField.IMG_URL].trim() &&
+    movieData[MovieField.TITLE].trim();
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={formKey} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={movieData.title}
+        onChange={handleChange(MovieField.TITLE)}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={movieData.description}
+        onChange={handleChange(MovieField.DESCRIPTION)}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={movieData.imgUrl}
+        onChange={handleChange(MovieField.IMG_URL)}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={movieData.imdbUrl}
+        onChange={handleChange(MovieField.IMDB_URL)}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={movieData.imdbId}
+        onChange={handleChange(MovieField.IMDB_ID)}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +93,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isFormValid}
           >
             Add
           </button>
---
+++ src/constants/MovieField.ts
@@ -0,0 +1,7 @@
+export enum MovieField {
+  TITLE = 'title',
+  DESCRIPTION = 'description',
+  IMG_URL = 'imgUrl',
+  IMDB_URL = 'imdbUrl',
+  IMDB_ID = 'imdbId',
+}
